f58b9c24370dfca4b99e740063fdac7af37bad5b
Katryna18 <55546767+Katryna18@users.noreply.github.com>
2019-10-10 23:40:46 -0500

    Update Ejercicio_Menu_Versionamiento.java
    Métodos creados

--- Ejercicio_Menu_Versionamiento/src/ejercicio_menu_versionamiento/Ejercicio_Menu_Versionamiento.java
+++ Ejercicio_Menu_Versionamiento/src/ejercicio_menu_versionamiento/Ejercicio_Menu_Versionamiento.java
@@ -10,113 +10,86 @@ import javax.swing.JOptionPane;
 
 public class Ejercicio_Menu_Versionamiento {
 
+   private int numero1;
+    private int numero2;
+    private double resultado;
    
-    public static void main(String[] args) {
-        //declarar variables
-       int op = 0;
-       do{
-           try{
-           op= Integer.parseInt(JOptionPane.showInputDialog(null, "1.Operaciones Básicas\n"
-                   + "2. Ejercicio de Arreglos\n"
-                   + "3. Ejercicio de Matrices\n"
-                   + "4. Salir"));
-           }catch(Exception ex){
-           JOptionPane.showMessageDialog(null, "Debe ingresar solo valores númericos");
-           }
-       switch(op){
-           case 1 ://Operadores
-               int opDos=0;
-               int numeroUno=4;
-               int numeroDos = 6;
-               int resultado;
-                do{
-                  
-           try{         
-           opDos= Integer.parseInt(JOptionPane.showInputDialog(null, "1.Suma\n"
-                   + "2. Resta\n"
-                   + "3. Multiplicaciòn\n"
-                   + "4. División\n"
-                   + "5. Volver al menú Anterior"));
-           }catch(Exception ex){
-           JOptionPane.showMessageDialog(null,"Debe ingresar solo valores númericos");
-           
-           }
-           switch(opDos){
-              case 1://Suma
-                  resultado = numeroUno+numeroDos;
-                  JOptionPane.showMessageDialog(null, "La suma del número "+ numeroUno+"+"+numeroDos+"="+resultado);
-                   break;
-               case 2:
-                   resultado = numeroUno-numeroDos;
-                  JOptionPane.showMessageDialog(null, "La resta del número "+ numeroUno+"-"+numeroDos+"="+resultado);
-                   break;
-               case 3:
-                   resultado = numeroUno*numeroDos;
-                  JOptionPane.showMessageDialog(null, "La Multiplicación del número "+ numeroUno+"*"+numeroDos+"="+resultado);
-                   break;
-               case 4: 
-                   resultado = numeroUno/numeroDos;
-                  JOptionPane.showMessageDialog(null, "La división del número "+ numeroUno+"/"+numeroDos+"="+resultado);
-                   break;
-               }
-                }while(opDos!=5);
-                
-               break;
-               
-               
-           case 2://Arreglos
-               
-               String claves [] = {"A","C","A","B","A"};
-               String respuestas[] = new String [5];
-        
-        //Declaro variables
-        int contador = 0;
-        int acumulador = 1;
-        
-        JOptionPane.showMessageDialog(null, "Responde con A,B o C las siguientes 5 preguntas");
-        for(int i = 0; i<claves.length; i++){
-        respuestas [i] = JOptionPane.showInputDialog(null,acumulador+". Por favor ingresa la respuesta");
-        acumulador++;
-        
-        if(respuestas[i].equalsIgnoreCase(claves[i])){
-        contador++;
-        
-        }
-        
-        }
-        
-        if(contador>=3){
-        JOptionPane.showMessageDialog(null, "Pasaste el examen, respondiste : " + contador + " respuestas verdaderas y " + (respuestas.length-contador)+ " incorrectas");
-               
-        
-        }else {
-        JOptionPane.showMessageDialog(null, "Perdiste el examen, respondiste " + contador + " respuestas verdaderas y " + (respuestas.length-contador) + " incorrectas");
-        
-        }
-        break;
-               
-           case 3: //Matrices
-               
-        String matriz[][] = new String[2][3];
-        //Cargamos el arreglo
-        for(int ejeX = 0; ejeX < matriz.length; ejeX++){
-            for(int ejeY = 0; ejeY < matriz[ejeX].length; ejeY++){
-                matriz[ejeX][ejeY] = JOptionPane.showInputDialog("Ingrese el valor para la posición [" + ejeX + "]["+ejeY+"]");
-            }
-        }
-        
-        //Mostrar los valores ingresados en la matriz
-        for(int ejeX = 0; ejeX < matriz.length; ejeX++){
-            for(int ejeY = 0; ejeY < matriz[ejeX].length; ejeY++){
-                JOptionPane.showMessageDialog(null, "El valor ingresado para la posición  [" + ejeX + "]["+ejeY+"]" + " es: " + matriz[ejeX][ejeY]);
-            }
-        }
-               
-               break;       
-       }     
-       }while(op!=4);
-    
-        
+   
+    public void setNumero1(int num1){
+        this.numero1 = num1;
     }
-    
+   
+    public int getNumero1(){
+        return this.numero1;
+    }
+   
+    public void setNumero2(int num2){
+        this.numero2 = num2;
+    }
+   
+    public int getNumero2(){
+        return this.numero2;
+    }
+   
+    public void setResultado(double result){
+        this.resultado = result;
+    }
+   
+    public double getResultado(){
+        return this.resultado;
+    }
+   
+    public int sumar(int num1, int num2){
+        this.setNumero1(num1);
+        this.setNumero2(num2);
+       return this.getNumero1() + this.getNumero2();
+    }
+   
+    public int restar(int num1, int num2){
+        this.setNumero1(num1);
+        this.setNumero2(num2);
+       return this.getNumero1() - this.getNumero2();
+    }
+   
+    public int multiplicar(int num1, int num2){
+        this.setNumero1(num1);
+        this.setNumero2(num2);
+       return this.getNumero1() * this.getNumero2();
+    }
+   
+    public double division(int num1, int num2){
+        this.setNumero1(num1);
+        this.setNumero2(num2);
+       return (double) this.getNumero1()/this.getNumero2();
+    }
+   
+    public void tablaMultiplicar(){
+        int tabla [][] = new int [10][3];
+        int numero;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digita el la tabla que quieras calcular"));
+       
+        for(int i=0 ; i<10 ; i++){
+                tabla[i][0]=numero;
+                tabla[i][1]=i+1;
+                tabla[i][2] = tabla[i][0] * tabla[i][1];
+        }
+        for(int i=0 ; i<10 ; i++){
+                JOptionPane.showMessageDialog(null,tabla[i][0]+" X "+tabla[i][1]+" = "+tabla[i][2]);
+        }  
+    }
+   
+    public void vector(){
+        String arregloDatos [] = new String[3];
+       
+                for(int i=0; i<arregloDatos.length;i++){
+                   
+                    arregloDatos[i]=JOptionPane.showInputDialog("Ingrese un valor tipo Texto en la posicion "+ i );
+                }
+       
+                for(int j=0;j<arregloDatos.length;j++){
+           
+                    JOptionPane.showMessageDialog(null,"El valor en la posicion "+ j + " es: "+ arregloDatos[j] );
+                }
+    }
+   
 }
