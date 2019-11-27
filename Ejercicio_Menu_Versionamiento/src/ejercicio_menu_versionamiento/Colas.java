794979712bf72a8f5be102f890a51351283b5765
KatheH <KatheH@Katherine>
2019-11-27 17:44:54 -0500

    Arreglos

---
+++ Ejercicio_Menu_Versionamiento/src/ejercicio_menu_versionamiento/Colas.java
@@ -0,0 +1,11 @@
+
+package ejercicio_menu_versionamiento;
+
+
+public class Colas {
+
+    void opcSiete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- Ejercicio_Menu_Versionamiento/src/ejercicio_menu_versionamiento/Operaciones_Basicas.java
+++ Ejercicio_Menu_Versionamiento/src/ejercicio_menu_versionamiento/Operaciones_Basicas.java
@@ -3,6 +3,10 @@ package ejercicio_menu_versionamiento;
 
 
 public class Operaciones_Basicas {
+
+    public void opcUno() {
+        
+    }
     
     
     
